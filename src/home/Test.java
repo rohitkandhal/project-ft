7f8de96a28a5596f13e03c870e2591adcb868811
Rohit Kandhal <rohit_kandhal@yahoo.co.in>
2013-10-19 15:38:14 -0400

    Get all subset of a set

--- src/home/Test.java
+++ src/home/Test.java
@@ -1,14 +1,27 @@
 package home;
 
+import java.util.ArrayList;
+
 public class Test {
 	public static void main(String[] args)
 	{
 		//System.out.println("Hello World");
 		
-		BitManipulation bitman = new BitManipulation();
+		// BitManipulation bitman = new BitManipulation();
 		// bitman.updateBits(1024, 21, 2, 6);
 		// bitman.doubleToBinary("32768.0");
-		bitman.nextSmallestNumber(35);
+		// bitman.nextSmallestNumber(35);
+		
+		// Recursion Problems
+		Recursion recursion = new Recursion();
+		ArrayList<Integer> inputSet = new ArrayList<Integer>();
+		inputSet.add(1);
+		inputSet.add(2);
+		inputSet.add(3);
+		ArrayList<ArrayList<Integer>> outputSet;
+		outputSet = recursion.GetSubsetCollection(inputSet);
+		recursion.printSubsetCollection(outputSet);
+		
 	}
 
 }
