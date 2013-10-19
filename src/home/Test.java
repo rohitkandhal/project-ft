18914c3b8627407dfef81943b9b50796f78553a2
Rohit Kandhal <rohit_kandhal@yahoo.co.in>
2013-10-19 17:33:00 -0400

    Overloaded HashCode and Equals method

--- src/home/Test.java
+++ src/home/Test.java
@@ -3,6 +3,43 @@ package home;
 import java.util.ArrayList;
 
 public class Test {
+	
+	private int nameId;
+	private String user;
+	
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + nameId;
+		result = prime * result + ((user == null) ? 0 : user.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (!(obj instanceof Test)) {
+			return false;
+		}
+		Test other = (Test) obj;
+		if (nameId != other.nameId) {
+			return false;
+		}
+		if (user == null) {
+			if (other.user != null) {
+				return false;
+			}
+		} else if (!user.equals(other.user)) {
+			return false;
+		}
+		return true;
+	}
+	
 	public static void main(String[] args)
 	{
 		//System.out.println("Hello World");
