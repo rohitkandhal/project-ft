47a6f5c4e2bc4d0ac04c5c73ec0f950fcd540aa9
Rohit Kandhal <rohit_kandhal@yahoo.co.in>
2013-11-02 02:23:25 -0400

    added few helper functions in test class

--- README.md
+++ README.md
@@ -9,3 +9,8 @@ A project having various small projects created for learning. List of all progra
 
 ### Recursion
 1. Get all subsets of a set.
+
+### Test
+1. Test Harness for reading user input using Buffered Reader.
+2. Simple Method to test regular expression (reges) in java.
+3. Equals and HashCode methods overloaded.
--- src/home/Test.java
+++ src/home/Test.java
@@ -1,12 +1,16 @@
 package home;
 
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Test {
-	
+
 	private int nameId;
 	private String user;
-	
+
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -15,6 +19,7 @@ public class Test {
 		result = prime * result + ((user == null) ? 0 : user.hashCode());
 		return result;
 	}
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj) {
@@ -39,30 +44,49 @@ public class Test {
 		}
 		return true;
 	}
-	
-	public static void main(String[] args)
-	{
-		//System.out.println("Hello World");
-		
-		 BitManipulation bitman = new BitManipulation();
+
+	public static void main(String[] args) throws Exception {
+		// System.out.println("Hello World");
+
+		// BitManipulation bitman = new BitManipulation();
 		// bitman.updateBits(1024, 21, 2, 6);
 		// bitman.doubleToBinary("32768.0");
 		// bitman.nextSmallestNumber(35);
 		// bitman.getEstimate("YRGB", "RGGB");
 		// bitman.getSimilarityCount("Rohit", "MOHIT");
-		
-		// Recursion Problems
-		
-		Recursion recursion = new Recursion();
-//		ArrayList<Integer> inputSet = new ArrayList<Integer>();
-//		inputSet.add(1);
-//		inputSet.add(2);
-//		inputSet.add(3);
-//		ArrayList<ArrayList<Integer>> outputSet;
-//		outputSet = recursion.GetSubsetCollection(inputSet);
-//		recursion.printSubsetCollection(outputSet);
 
-		recursion.printInvertedTriangle();
+		// Recursion Problems
+
+		// Recursion recursion = new Recursion();
+		// ArrayList<Integer> inputSet = new ArrayList<Integer>();
+		// inputSet.add(1);
+		// inputSet.add(2);
+		// inputSet.add(3);
+		// ArrayList<ArrayList<Integer>> outputSet;
+		// outputSet = recursion.GetSubsetCollection(inputSet);
+		// recursion.printSubsetCollection(outputSet);
+
+		// recursion.printInvertedTriangle();
+
+		Test t1 = new Test();
+		t1.regexTest();
 	}
 
+	public void testHarness() throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String line = br.readLine();
+		int N = Integer.parseInt(line);
+		for (int i = 0; i < N; i++) {
+			System.out.println("hello world");
+		}
+	}
+
+	public void regexTest() {
+		
+		Pattern p = Pattern.compile("Foo"); 
+		Matcher m = p.matcher("FooGooFooooHelloFootestingfoo");
+		while (m.find()) {
+			System.out.println(m.start() + ", " + m.end() + ", " + m.group());
+		}
+	}
 }
